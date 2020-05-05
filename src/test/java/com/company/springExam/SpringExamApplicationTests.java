6a67c8fa2a50247bef70171e9a60ea5cae99b01e
Nuriza <nuriza.samidinovna@gmail.com>
2020-05-05 19:52:02 +0600

    first commit

---
+++ src/test/java/com/company/springExam/SpringExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.springExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
